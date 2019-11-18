4ae17e046d3d6ecbb7976044cad38753946b8bce
ljungqvist93 <ljungqvist1993@gmail.com>
2019-11-18 18:24:43 +0100

    About me-page updated.

--- about.php
+++ about.php
@@ -2,8 +2,40 @@
 <main id="about">
     <div class="wrapper">
         <h1>
-            Coming soon!
+            I'm Daniel - a nerdy web developer from Sweden with a huge love for writing.
         </h1>
+
+        <p>
+        	For as long as I can remember, I've wanted to make my own website, where I could write about
+        	technology, and at times other stuff that just comes into my head. Why haven't it happened until
+        	now, you might ask? to be honest - I got no idea. I've always just put it on hold, or worst still,
+        	I've began the website, wanting a WAY to ambitious project, and ended out loosing interest
+        	because things doesn't result in the way I want.
+        </p>
+        <p>
+        	My goul with this site was extremly simple. Build something simple, easy and fast to navigate, and
+        	focus wholeheartedly on creating and delivering oustanding contant, time after time.
+        </p>
+        <h2>Experience</h2>
+        <p>
+        	I don't have any education for what I do, and what I'm interested in. I love to create websites,
+        	and I've build this site from scratch with HTML, JavaScript (jQuery), CSS and a bit of PHP. I'm first
+        	and foremost a front-end developer, and I use Sublime Text 3 with a few extensions to make my
+        	coding experience a bit easier.
+        </p>
+        <p>
+        	I'm also on GitHub - here's my website's repository.
+        </p>
+        <p>
+        	I'm do not have an education for anything I do. I've learned all stuff web development from years
+        	and years of bascially trying until I succeed. 
+        </p>
+        <p>
+        	__________________________________
+        </p>
+        <p>
+        	I'm planning on updating this page further in the future. For now tho, this'll do :)
+        </p>
     </div>
 </main>
 <?php include 'assets/parts/bottom.php'; ?>

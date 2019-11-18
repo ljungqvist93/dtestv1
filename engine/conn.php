d8d5025789dc9313b494ccb4d0f08ddb16712049
ljungqvist93 <ljungqvist1993@gmail.com>
2019-11-18 17:48:11 +0100

    Folder.

--- engine/conn.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-	//$db = new PDO('mysql:host=127.0.0.1;dbname=danielljungqvist;charset=latin1', 'root', 'Daniel93');
-    $db = new PDO('mysql:host=127.0.0.1;dbname=danielljungqvist;charset=latin1', '', '');
